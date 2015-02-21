73d84a12b26c2dabbaae423b668be8e11f1c5a1f
kobbesun <riyutian@gmail.com>
2015-02-21 16:04:09 +0800

    final code

--- server.R
+++ server.R
@@ -1,10 +1,34 @@
 library(shiny)
+require(stats)
+data(mtcars)
 
-diabetesRisk <- function(glucose) glucose / 200
+fit.exp <- lm(mpg ~ factor(cyl) + hp + wt, data = mtcars)
+similar_models <- function (predict_mpg) {
+        similar_ones <- subset(mtcars, mpg > predict_mpg - 1 & mpg < predict_mpg + 1)
+        if (nrow(similar_ones) == 0) { 
+                "No model with similar MPG found"
+        } else {
+                similar_ones
+        }
+}
+
 
 shinyServer(
         function(input, output) {
-                output$inputValue <- renderPrint({input$glucose})
-                output$prediction <- renderPrint({diabetesRisk(input$glucose)})
+                output$inputs <- renderText({ paste('Weight', input$wt, 
+                                                    'thousand lb - horsepower', input$hp,
+                                                    '- number of cylinder', input$cyl)
+                })
+                
+                output$coefficients <- renderPrint({coefficients(fit.exp)})
+                predict_mpg <- reactive({predict(fit.exp, data.frame(cyl = input$cyl,
+                                                                     hp = input$hp,
+                                                                     wt = input$wt))
+                })
+                
+                output$mpg <- renderText({predict_mpg()})
+                
+                output$models <- renderPrint({similar_models(predict_mpg())})
+
         }
 )
--- ui.R
+++ ui.R
@@ -2,18 +2,49 @@ library(shiny)
 shinyUI(
         pageWithSidebar(
                 # Application title
-                headerPanel("Diabetes prediction"),
+                headerPanel("Moto MPG Projection as of 1974 in the US"),
                 
                 sidebarPanel(
-                        numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5),
-                        submitButton('Submit')
+                        sliderInput('wt', 'Weight (lb/1000)',value = 2, min = 1.2, 
+                                    max = 5.5, step = 0.05),
+                        sliderInput('hp', 'Gross horsepower',value = 150, min = 50, 
+                                    max = 350, step = 1),
+                        #sliderInput('cyl', 'Number of cylinders',value = 6, min = 4, 
+                        #            max = 8, step = 2),
+                        selectInput('cyl', 'Number of cylinders', c(4, 6, 8), selected = 6)
+                        #submitButton('Submit')
+                        #actionButton("goButton", "Go!")
                 ),
                 mainPanel(
-                        h3('Results of prediction'),
                         h4('You entered'),
-                        verbatimTextOutput("inputValue"),
+                        verbatimTextOutput("inputs"),
+                        h4('Coefficients of the multiple linear model'),
+                        verbatimTextOutput("coefficients"),
                         h4('Which resulted in a prediction of '),
-                        verbatimTextOutput("prediction")
+                        verbatimTextOutput("mpg"),
+                        h4('1974 motor models with similar MPG (+/-1 mpg) are'),
+                        verbatimTextOutput("models"),
+                        br(),
+                        h4('User Mannual'),
+                        div('The shiny app borrowed the multiple regression 
+                             model of Coursera course, Regression Models. I built a 
+                             multiple regression model based on the data of
+                             "Motor Trend Car Road Test" from 1974 Motor Trend US
+                             magazine. After data analysis, I chose three independent
+                             variables, weight, gross housepower, and number of cylinders,
+                             to predict miles/gallon of a motor.'),
+                        br(),
+                        div('I created three inputs widgets for independent variables
+                             in sidebarPanel. The shiny app displays user inputs, model 
+                             coefficients, and model prediction result. At last the app 
+                             shows the motor models that have mpg within the +/-1 
+                             mpg range of predicted mpg. Of course the motor models are
+                             from the "Motor Trend Car Road Test" data set.'),
+                        br(),
+                        div('Github repository: '),
+                        div(a('https://github.com/kobbesun/devprod_pa')),
+                        br(),
+                        div(' ~ END ~')
                 )
         )
 )
